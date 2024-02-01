021954599550e42f3a6425abd193266484118bd1
Coleton Watt <electricninja102@gmail.com>
2024-01-31 20:16:52 -0700

    Program03
    Completed the basic code of program 03 assignment, prog03 creates as many process as input, each process runs minMax which compares numbers in a binary file. binaryFileGenerator generates said file.

---
+++ Assignments/program03/minMax.c
@@ -0,0 +1,61 @@
+/*
+Author: Coleton Watt
+Date: Jan 31, 2022
+It took me about 2 hours
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+int main(int argc, char *argv[]) {
+	//printf("HELLO!!\n");
+	clock_t start = clock();
+	struct timeval startTime, endTime;
+	gettimeofday(&startTime, NULL);
+	
+	if(argc < 2){
+		printf("Usage: readfile <filename>\n");
+		return -1;
+	}
+	FILE * inputFile;
+	inputFile= fopen(argv[1], "rb"); //read binary file
+	if(!inputFile){
+		printf("File Not Found");
+		return -1;
+	}
+	//int pid = getpid();
+	fseek(inputFile, 0, SEEK_END); //go to end of file
+	long size = ftell(inputFile);
+	fseek(inputFile, 0, SEEK_SET); //go to start of file
+	int numberOfIntegers = (int)size / (int) sizeof(int); //the number of int in the file
+//	printf("file size: %li bytes\n", size);
+//	printf("sizeof(int) = %i bytes\n",(int) sizeof(int));
+//	printf("how many integers = %i\n\n", numberOfIntegers);	
+	
+	//int totalAmount = 0;
+	int minNumber = 0;
+	int maxNumber = 0;
+
+	for(int i = 0; i < numberOfIntegers; i++){
+		int temp = 0;
+		fread(&temp,sizeof(int),1,inputFile);
+		//totalAmount +=temp;
+		minNumber = minNumber > temp ? temp: minNumber;
+		maxNumber = maxNumber < temp ? temp: maxNumber;
+
+	}
+	fclose(inputFile);  // close the file now that we're done
+
+	float cpuTime = (clock() - start);
+	gettimeofday(&endTime, NULL);
+	double wallTime = (double) (endTime.tv_usec - startTime.tv_usec) /1000000 +
+	(double) (endTime.tv_sec - startTime.tv_sec);
+	
+	printf("Child #:%s\t| pid: %d\t| min:%d\t| max: %d\t| Wall(sec): %f\t| CPU(sec) %f\n", argv[2], (int)getpid(), minNumber, maxNumber, wallTime, ((cpuTime)/CLOCKS_PER_SEC));
+	
+	return 0;
+	
+}
---
+++ Assignments/program03/prog03.c
@@ -0,0 +1,71 @@
+/*
+Author: Coleton Watt
+Date: Jan 31, 2022
+It took me about 2 hours
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <sys/time.h>
+
+#define NAME "Coleton Watt"
+int main(int argc, char *argv[]) {
+	clock_t start = clock();
+	struct timeval startTime, endTime;
+	gettimeofday(&startTime, NULL);
+	int cp[2]; //pipe array 0 - read 1 - write
+
+	if(argc < 3){
+		printf("Missing file name or process count");
+		return -1;
+	}
+	if(pipe(cp)){
+		printf("didn't work, couldn't not establish pipe.\n");
+		return -1;
+	}
+	
+	printf("My name is: %s\n", NAME);
+	int processID;
+	for(int processCounter = 0; processCounter < atoi(argv[1]); processCounter++){
+		processID = fork();
+		if(processID == 0){
+			close(0); //close stdin
+			close(1); //close stdout
+			close(cp[0]); //close write pipe in
+			dup2(cp[1], 1); //move stdout to pipe of cp[1]
+			char str[sizeof(int)];
+			sprintf(str, "%d", processCounter);
+
+			execl("minMax", "minMax", argv[2], str, (char*)0);
+			
+
+		}else{
+			
+		}
+	}
+	if(processID != 0){
+		wait(NULL);
+
+		char ch;
+		close(cp[1]);
+		while(read(cp[0], &ch, 1) == 1) {
+			printf("%c",ch);
+		}
+		close(cp[0]);
+
+
+		float cpuTime = (clock() - start);
+		gettimeofday(&endTime, NULL);
+		double wallTime = (double) (endTime.tv_usec - startTime.tv_usec) /1000000 +
+		(double) (endTime.tv_sec - startTime.tv_sec);
+		
+		printf("Main process CPU time: %f\n", cpuTime/CLOCKS_PER_SEC);
+		printf("Wall time: %f\n", wallTime);
+	}
+	return 0;
+}
